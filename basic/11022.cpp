9a63bf4a69ba81f4b471a70b4832a03b22ccfca5
KwonHyeMin <41566131+KwonHyeMin@users.noreply.github.com>
2020-03-15 20:45:57 +0900

    A+B -8

--- basic/11022.cpp
+++ basic/11022.cpp
@@ -1,5 +1,12 @@
 //언젠가 A+B -9도전해보기! <- 이 문제는 입력이 엄청 다양한 경우이므로 변수 자료형의 입력 크기 등에 대해 많이 생각해보기!
 
+/*
+cin : 공백문자일때 입력을 끊는다
+getline : 공백문자가 있는 문장도 잘 받아서 출력한다
+
+
+*/
+
 #include <iostream>
 #include <cstdio>
 #include <string>
